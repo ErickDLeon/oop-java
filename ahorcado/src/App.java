1b91d6f56b5dc9b25ea1c13492eb969e292d285e
erickleon <erickl.solemti@coppel.com>
2022-10-05 22:50:31 -0500

    delete settings vscode

--- ahorcado/src/App.java
+++ ahorcado/src/App.java
@@ -2,7 +2,7 @@ import com.game.bo.Game;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Ahorcado!, ingresa: 0 para salir");
+        System.out.println("Hello, Ahorcado!!, ingresa: 0 para salir");
 
         Game g = new Game();
 
